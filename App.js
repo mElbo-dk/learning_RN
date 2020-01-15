0250eff1aaaf1de68bb71932a864b2ae094dc3f2
mElbo <mortenelbo01@gmail.com>
2020-01-15 00:09:45 +0000

    with modal for input page

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
 import React, { useState } from 'react'
-import { StyleSheet, View, FlatList } from 'react-native'
+import { StyleSheet, View, FlatList, Button } from 'react-native'
 import TextItem from './components/TextItems'
 import TextItemInput from './components/TextItemInput'
 
 export default function App() {
   // state is in hooks set to array
   const [enteredList, setEnteredList] = useState([])
+  const [isAddMode, setIsAddMode] = useState('')
 
   // the ramdom is to make uniqe numbers for the key to FlatList
   const addTextHandeler = ItemText => {
     setEnteredList(enteredList => [...enteredList, 
       { id: Math.random().toString(), value: ItemText }])
+    setIsAddMode(false)
   }
+
+  const cancelAddTextHandeler = () => {
+    setIsAddMode(false)
+  }
+
 
   const deleteTextHandeler = idTextFilter => {
     setEnteredList(enteredList => { 
@@ -22,15 +29,14 @@ export default function App() {
   // render the screen 
   return (
     <View style={styles.screen}>
-      
-      <TextItemInput onAddText={addTextHandeler}/>
+      <Button  title='Add new text' onPress={ () => setIsAddMode(true)}/>
+      <TextItemInput visible={isAddMode} onAddText={addTextHandeler} cancel={cancelAddTextHandeler}/>
       <FlatList 
         data={enteredList}
         renderItem={itemData => 
           <TextItem 
             id={itemData.item.id}
             onDelete={deleteTextHandeler} 
-            
             item={itemData.item.value}/>}
       />
     </View>
--- components/TextItemInput.js
+++ components/TextItemInput.js
@@ -1,40 +1,57 @@
 import React , { useState } from 'react'
-import { View, TextInput, StyleSheet,Button } from 'react-native'
+import { View, TextInput, StyleSheet,Button,Modal } from 'react-native'
 
 // creating a object function
 const TextItemInput = props => {
   // setting the state 
   const [enteredTextValue, setEnteredText] = useState('')
+  
+
   // text input handler for listening for keys 
   const textInputHandler = (enteredText) => {
     setEnteredText(enteredText)
   }
-  
+
+  const addTextHandeler = () =>{
+    props.onAddText(enteredTextValue)
+    setEnteredText('')
+  }
+
+
   // render and send to the app.js
   // the .bind() is to send value together with the onPress props.
   return (
-    <View style={styles.inputContainer}>
-      <TextInput 
-        placeholder  = 'Input text' 
-        style={styles.input}
-        onChangeText={textInputHandler}
-        value={enteredTextValue}/>
-      <Button title='Add' onPress={props.onAddText.bind(this, enteredTextValue)}/>
-    </View>
+    <Modal visible={props.visible} animationType="slide">
+      <View style={styles.inputContainer}>
+        <TextInput 
+          placeholder  = 'Input text' 
+          style={styles.input}
+          onChangeText={textInputHandler}
+          value={enteredTextValue}/>
+        <View style={styles.inLine}>
+          <Button title='Cancel' color='orange' onPress={props.cancel}/>
+          <Button title='Add' onPress={addTextHandeler}/>
+        </View>
+      </View>
+    </Modal>
   )
 }
 
 // style sheet creation 
 const styles = StyleSheet.create({
   inputContainer: { 
-    flexDirection: 'row' ,
-    justifyContent: 'space-evenly',
-    alignItems: 'baseline'
+    flex: 1 ,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   input: { width: '80%' ,
     borderColor: 'blue' ,
     borderWidth: 1 ,
-    padding: 10 
+    padding: 10 ,
+    marginBottom: 10
+  },
+  inLine: {
+    flexDirection: 'row'
   }
 })
 
